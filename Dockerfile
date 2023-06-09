4e359cff52c3e3a65689cea3f663583566964bf9
roman <roman.rubinshtein@1win.pro>
2023-06-09 11:44:08 +0200

    fixed dockerFile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,35 @@
-FROM node:18-alpine3.16
+FROM node:latest
 
 WORKDIR /app
+
+# Установка зависимостей Puppeteer
+RUN apt-get update \
+    && apt-get install -y \
+        libnss3 \
+        libatk1.0-0 \
+        libatk-bridge2.0-0 \
+        libx11-xcb1 \
+        libxcb1 \
+        libxcomposite1 \
+        libxdamage1 \
+        libxi6 \
+        libxtst6 \
+        libgtk-3-0 \
+        libgdk-pixbuf2.0-0 \
+        libpango-1.0-0 \
+        libcairo2 \
+        libfontconfig1 \
+        libgdk-pixbuf2.0-0 \
+        libgbm1 \
+        libasound2 \
+        libxss1 \
+        fonts-liberation \
+        libappindicator1 \
+        libappindicator3-1 \
+        gconf-service \
+        libdbus-glib-1-2 \
+        xvfb \
+    && rm -rf /var/lib/apt/lists/*
 
 COPY package*.json ./
 
--- src/index.js
+++ src/index.js
@@ -3,41 +3,44 @@ import 'dotenv/config'
 import axios from 'axios'
 import cron from 'node-cron'
 import puppeteer from 'puppeteer'
-import { BOTID, CHANNELID } from './config'
+import { BOTID, CHANNELID } from './config.js'
 
 const bot = new TelegramBot(BOTID, { polling: true, filepath: false })
-
 cron.schedule('* * * * *', async () => {
   async function beribitSite () {
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    })
-    const page = await browser.newPage()
-    await page.goto('https://beribit.com/exchange/spots/USDT_RUB')
-    const client = await page.target().createCDPSession()
-    await client.send('Network.enable')
+      args: [
+        "--disable-gpu",
+        "--disable-dev-shm-usage",
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+      ]
+    });
+    const page = await browser.newPage();
+    await page.goto('https://beribit.com/exchange/spots/USDT_RUB');
+    const client = await page.target().createCDPSession();
+    await client.send('Network.enable');
     return new Promise((resolve, reject) => {
       client.on('Network.webSocketFrameReceived', async (event) => {
-        const json = JSON.parse(event.response.payloadData)
+        const json = JSON.parse(event.response.payloadData);
         if (json.Depth) {
           const beribit = json.Depth.Asks.slice(0, 2).map(item => {
             return { rate: item.ExchangeRate }
           })
-          await browser.close()
-          resolve(beribit)
+          await browser.close();
+          resolve(beribit);
         }
       })
     })
   }
   const beribit = await beribitSite();
-  console.log(beribit)
-  const averageBeribit = Number(((beribit[0].rate + beribit[1].rate) / 2).toFixed(3))
+  const averageBeribit = Number(((beribit[0].rate + beribit[1].rate) / 2).toFixed(3));
 
   const body = { fiat: 'RUB', page: 1, rows: 10, tradeType: 'buy', asset: 'USDT', countries: [], proMerchantAds: false, publisherType: null, payTypes: ['RaiffeisenBank'] }
   const ddd = await axios.post('https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search', body)
-  const result = ddd.data.data
-  const newarr = result.slice(1, 3)
+  const result = ddd.data.data;
+  const newarr = result.slice(1, 3);
   const binance = newarr.map(item => {
     return {
       rate: item.adv.price,
@@ -45,18 +48,18 @@ cron.schedule('* * * * *', async () => {
     }
   })
 
-  const averageBinance = Number(((Number(binance[0].rate) + Number(binance[1].rate)) / 2).toFixed(3))
+  const averageBinance = Number(((Number(binance[0].rate) + Number(binance[1].rate)) / 2).toFixed(3));
   // console.log(beribit);
   // console.log(binance)
   // console.log(averageBeribit);
   // console.log(averageBinance);
 
-  const diff = Number(((100 - averageBeribit / averageBinance * 100) - 0.1 - 0.08).toFixed(3))
-  console.log(diff)
+  const diff = Number(((100 - averageBeribit / averageBinance * 100) - 0.1 - 0.08).toFixed(3));
+  console.log(diff);
   if (diff >= 0.3) {
-    const message = `<b>SOS</b>\u203C\uFE0F\n\n Спред больше 0,3\n\nСпред:${diff}\n\n<b>Beribit</b>: ${beribit[0].rate}, ${beribit[1].rate}\n\nBinance: ${binance[0].rate} - ${binance[0].nick}\n${binance[1].rate} - ${binance[1].nick}`
+    const message = `<b>SOS</b>\u203C\uFE0F\n\n Спред больше 0,3\n\nСпред:${diff}\n\n<b>Beribit</b>: ${beribit[0].rate}, ${beribit[1].rate}\n\nBinance: ${binance[0].rate} - ${binance[0].nick}\n${binance[1].rate} - ${binance[1].nick}`;
     await bot.sendMessage(CHANNELID, message, {
       parse_mode: 'HTML'
-    })
+    });
   }
-})
+});
